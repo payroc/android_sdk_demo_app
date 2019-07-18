42c8eaebfb06a4987fc2ae084e1e95c05fd9a445
Preston Farr <preston.farr@payroc.com>
2019-07-18 12:45:51 -0600

    IMOB-208 - Added Crashalytics

--- app/src/main/java/com/payroc/payrocsdktestapp/MainActivity.kt
+++ app/src/main/java/com/payroc/payrocsdktestapp/MainActivity.kt
@@ -17,17 +17,20 @@ import com.payroc.sdk.enums.TxnModes
 import com.payroc.sdk.models.LineItem
 import com.payroc.sdk.models.Transaction
 import com.payroc.sdk.ui.numberpad.NumberPadActivity
-import com.payroc.sdk.ui.numberpad.NumberPadFragment
 import com.payroc.sdk.ui.review.TxnReviewActivity
 import com.payroc.sdk.ui.signature.SignatureActivity
 import com.payroc.sdk.ui.tip.TipActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import java.math.BigDecimal
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Fabric.with(this, Crashlytics())
+
         setContentView(R.layout.activity_main)
 
 //        if (savedInstanceState == null) {
