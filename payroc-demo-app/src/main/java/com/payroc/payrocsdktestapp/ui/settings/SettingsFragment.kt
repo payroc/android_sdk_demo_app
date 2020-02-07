c8ab8ca893ab1ee2f44b21f079ced63686e67156
Preston Farr <preston.farr@payroc.com>
2020-02-06 17:18:33 -0700

    Upped to version 1.3.2 - added gateway ID support
    Signed-off-by: Preston Farr <preston.farr@payroc.com>

--- payroc-demo-app/src/main/java/com/payroc/payrocsdktestapp/ui/settings/SettingsFragment.kt
+++ payroc-demo-app/src/main/java/com/payroc/payrocsdktestapp/ui/settings/SettingsFragment.kt
@@ -1,25 +1,28 @@
 package com.payroc.payrocsdktestapp.ui.settings
 
 import android.app.Activity
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.appcompat.widget.AppCompatButton
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.payroc.payrocsdktestapp.BuildConfig
 import com.payroc.payrocsdktestapp.R
 import com.payroc.sdk.PLog
-import stringLiveData
 import com.payroc.sdk.enums.Environment
 import com.payroc.sdk.enums.Gateways
+import stringLiveData
 
 class SettingsFragment : Fragment() {
 
@@ -32,6 +35,7 @@ class SettingsFragment : Fragment() {
     private lateinit var prefs: SharedPreferences
     private lateinit var apiUsername: EditText
     private lateinit var apiPassword: EditText
+    private lateinit var gatewayId: EditText
     private lateinit var environmentSpinner: Spinner
     private lateinit var gatewaySpinner: Spinner
     private lateinit var updateButton: AppCompatButton
@@ -45,6 +49,7 @@ class SettingsFragment : Fragment() {
         // TODO - validate the username and password at some point.
         apiUsername = view.findViewById(R.id.apiUsername)
         apiPassword = view.findViewById(R.id.apiPassword)
+        gatewayId = view.findViewById(R.id.gatewayId)
         environmentSpinner = view.findViewById(R.id.settingsEnvironmentSpinner)
         environmentSpinner.adapter = ArrayAdapter<Environment>(context!!, R.layout.support_simple_spinner_dropdown_item, Environment.values())
         gatewaySpinner = view.findViewById(R.id.settingsGatewaySpinner)
@@ -64,15 +69,12 @@ class SettingsFragment : Fragment() {
 
         setupFieldObserver(getString(com.payroc.sdk.R.string.shared_prefs_api_username_key), viewModel.apiUsername, apiUsername)
         setupFieldObserver(getString(com.payroc.sdk.R.string.shared_prefs_api_password_key), viewModel.apiPassword, apiPassword)
+        setupFieldObserver(getString(com.payroc.sdk.R.string.shared_prefs_gateway_id), viewModel.gatewayId, gatewayId)
         setupFieldObserver(getString(com.payroc.sdk.R.string.shared_prefs_api_environment_key), viewModel.apiEnvironment, environmentSpinner)
         setupFieldObserver(getString(com.payroc.sdk.R.string.shared_prefs_api_gateway_key), viewModel.apiGateway, gatewaySpinner)
     }
 
     private fun setupFieldObserver(prefKey: String, modelField: MutableLiveData<Int>, uiField: Spinner){
-        // TODO - for some reason its barfing on the int object somewhere its getting mutated to a string. fix this to watch results
-//        prefs.intLiveData(prefKey, 0).observe(this, Observer<Int> { position ->
-//            modelField.value = position
-//        })
         modelField.value = prefs.getInt(prefKey, 0)
         uiField.setSelection(modelField.value!!)
     }
@@ -88,6 +90,7 @@ class SettingsFragment : Fragment() {
     private fun updateCredentials(){
         prefs.edit().putString(getString(com.payroc.sdk.R.string.shared_prefs_api_username_key), apiUsername.text.toString()).apply()
         prefs.edit().putString(getString(com.payroc.sdk.R.string.shared_prefs_api_password_key), apiPassword.text.toString()).apply()
+        prefs.edit().putString(getString(com.payroc.sdk.R.string.shared_prefs_gateway_id), gatewayId.text.toString()).apply()
         prefs.edit().putInt(getString(com.payroc.sdk.R.string.shared_prefs_api_environment_key), environmentSpinner.selectedItemPosition).apply()
         prefs.edit().putInt(getString(com.payroc.sdk.R.string.shared_prefs_api_gateway_key), gatewaySpinner.selectedItemPosition).apply()
 
--- payroc-demo-app/src/main/java/com/payroc/payrocsdktestapp/ui/settings/SettingsViewModel.kt
+++ payroc-demo-app/src/main/java/com/payroc/payrocsdktestapp/ui/settings/SettingsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 class SettingsViewModel : ViewModel() {
     val apiUsername: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val apiPassword: MutableLiveData<String> by lazy { MutableLiveData<String>() }
+    val gatewayId: MutableLiveData<String> by lazy { MutableLiveData<String>() }
     val apiEnvironment: MutableLiveData<Int> by lazy { MutableLiveData<Int>() }
     val apiGateway: MutableLiveData<Int> by lazy { MutableLiveData<Int>() }
 }
